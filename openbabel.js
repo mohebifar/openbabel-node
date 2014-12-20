91dc7ea1a00caf15a657c88a877d83f8b25eecbe
mohebifar <netfars@gmail.com>
2014-12-20 13:12:28 +0330

    Example Javascript file

--- openbabel.js
+++ openbabel.js
@@ -1,11 +1,22 @@
+var start = Date.now();
 var openbabel = require('./build/Release/openbabel');
-var result = new openbabel.Conversion().setInFormat("smiles").read("C(N)(P)");
+var fs = require('fs');
 
+console.log("Require", Date.now() - start);
+var conversion = new openbabel.Conversion();
 
-console.log(result.molWeight);
-console.log(result.atomsCount);
+console.log("Create Conversion", Date.now() - start);
+var mol = conversion.setInFormat('smiles').read("CC");
 
-console.log(result.energy);
-console.log(result.isChiral());
-console.log(result.addHydrogens());
-console.log(result.atomsCount);
+console.log("Result is", mol.molWeight);
+
+var atoms1 = mol.getAtoms();
+
+var ff = openbabel.ForceField.findForceField("mmff94");
+console.log("Energy", ff.energy);
+ff.setup(mol);
+console.log("Energy", ff.energy);
+ff.systematicRotorSearch(1);
+console.log("Energy", ff.energy);
+fs.writeFileSync('a.mol', conversion.setOutFormat('mol').write(mol));
+console.log("Read", Date.now() - start);
