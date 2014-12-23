285427deb809181f383b2a3e3240081fca8ac3df
mohebifar <netfars@gmail.com>
2014-12-23 17:17:56 +0330

    JavaScript Lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var yuidoc = require("gulp-yuidoc");
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
-gulp.task('default', function() {
+gulp.task('lint', function() {
+    return gulp.src('lib/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('doc', function() {
     gulp.src("./lib/*.js")
         .pipe(yuidoc())
         .pipe(gulp.dest("./doc"));
+});
+
+gulp.task('watch', function() {
+    gulp.watch("./lib/*.js", ['lint', 'doc']);
 });
