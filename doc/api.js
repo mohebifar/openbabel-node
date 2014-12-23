850ba3f471536c8a583ca33b051d4354850048a9
mohebifar <netfars@gmail.com>
2014-12-23 18:18:58 +0330

    Add API Documentation

---
+++ doc/api.js
@@ -0,0 +1,23 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Atom",
+        "Bond",
+        "Builder",
+        "Conversion",
+        "ForceField",
+        "Molecule",
+        "Vector"
+    ],
+    "modules": [
+        "openbabel"
+    ],
+    "allModules": [
+        {
+            "displayName": "openbabel",
+            "name": "openbabel",
+            "description": "The main module for binding Openbabel to Node.js"
+        }
+    ]
+} };
+});
