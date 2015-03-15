540858c1f44941301f41d992dfc51ce7df3b4e2b
mohamad <netfars@gmail.com>
2015-03-15 18:41:02 +0330

    feat: JSON Reader functionality

--- examples/basic/mol-json.js
+++ examples/basic/mol-json.js
@@ -5,4 +5,9 @@ var mol = new ob.Conversion().setInFormat('smiles').read('CCCC');
 var builder = new ob.Builder();
 builder.build(mol);
 
-console.log(JSON.stringify(mol));
+var json = JSON.stringify(mol);
+var object = JSON.parse(json);
+
+var newMol = ob.Molecule.fromJSON(object);
+
+console.log(newMol.atomsCount);
